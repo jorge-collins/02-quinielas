065d451ae7168e59ea483424c204b7adb57fdfee
jorge-collins <jorge.collins@gmail.com>
2022-11-23 17:26:56 -0700

    feat: darktheme.js added.

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import { NextUIProvider } from '@nextui-org/react';
+import { darkTheme } from '../themes/';
 
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
     return (
-        <NextUIProvider>
+        <NextUIProvider theme={ darkTheme }>
             <Component {...pageProps} />
         </NextUIProvider>
     );
